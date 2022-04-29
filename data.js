794cb27b7a22e439cf6ed12bc81f110b5e2bb7ec
learningreac <learningreact2020@gmai.com>
2022-04-29 01:27:33 -0700

    index

---
+++ data.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+var projectlist = [
+    {
+        name:"Teams UI clone",
+        image_src: "https://raw.githubusercontent.com/learningreac/teams_clone/master/public/imgs/UI-with-routing.png",
+        github_address:"https://github.com/learningreac/teams_clone"
+    },
+    {
+        name:"Chat",
+        image_src: "https://raw.githubusercontent.com/learningreac/chat/master/public/imgs/Step6_Chat_page.png",
+        github_address:"https://github.com/learningreac/chat"
+    },
+    {
+        name:"Movie Search",
+        image_src: "https://raw.githubusercontent.com/learningreac/movie_app/master/public/imgs/home_page.png",
+        github_address:"https://github.com/learningreac/movie_app"
+    },
+    {
+        name:"Anecdotes with Redux",
+        image_src: "https://raw.githubusercontent.com/learningreac/fullstack_open/master/part6/redux-anecdotes/public/home.png",
+        github_address:"https://github.com/learningreac/fullstack_open/tree/master/part6"
+    },
+    {
+        name:"Delaunator",
+        image_src: "https://raw.githubusercontent.com/learningreac/ipc2581_react2/master/imgs/delaunator.png",
+        github_address:"https://github.com/learningreac/ipc2581_react2"
+    },
+    {
+        name:"XML parser",
+        image_src: "https://raw.githubusercontent.com/learningreac/ipc2581_react2/master/imgs/ipc2581Sample.png",
+        github_address:"https://github.com/learningreac/ipc2581_react2"
+    },
+    {
+        name:"React basic",
+        image_src: "https://raw.githubusercontent.com/learningreac/let_me_react/master/public/imgs/home_page.png",
+        github_address:"https://github.com/learningreac/let_me_react"
+    },
+    {
+        name:"Phonebook deployed",
+        image_src: "https://raw.githubusercontent.com/learningreac/fullstack_open/master/part2/phonebook_frontend/public/home.png",
+        github_address:"https://gentle-tundra-28997.herokuapp.com/"
+    },
+]
